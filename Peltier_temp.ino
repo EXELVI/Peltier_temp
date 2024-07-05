bb56be1cf1960f608b9fc881978d9b035cf81fd0
EXELVI <exelvi@outlook.com>
2024-07-05 21:08:11 +0200

    refactor: Removed DS18B20  and used a digital temperature sensor, display on OLED screen

--- Peltier_temp.ino
+++ Peltier_temp.ino
@@ -4,14 +4,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 2
-
-OneWire oneWire(ONE_WIRE_BUS);
-
-DallasTemperature sensors(&oneWire);
+byte sensorPin = A0;
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
@@ -37,14 +31,40 @@ void setup()
   display.display();
 
   delay(1000);
-
-  sensors.begin();
 }
 
 void loop()
 {
 
-  sensors.requestTemperatures();
+  float analoggg;
+  float Resistance;
+  analoggg = analogRead(sensorPin);
+  Resistance = (1023 / analoggg) - 1;
+  Resistance = 10000 / Resistance;
+  Serial.print(analoggg);
+  Serial.print(": ");
+  Serial.print(Resistance);
+  Serial.println(" Ohm");
 
-  Serial.println(sensors.getTempCByIndex(0));
+  float celsi;
+  celsi = Resistance / 10000;
+  celsi = log(celsi);
+  celsi /= 3950;
+  celsi += 1.0 / (25 + 273.15);
+  celsi = 1.0 / celsi;
+  celsi -= 273.15;
+
+  Serial.print(celsi);
+  Serial.println(" C");
+
+  
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 9);
+  display.println(String(celsi) + " C");
+  display.setTextSize(1);
+  display.println(String(Resistance) + " Ohm");
+  display.display();
+
 }
