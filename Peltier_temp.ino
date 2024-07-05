4fa9dafee4d4aa877ef080d7b7f9ba54ce99285e
EXELVI <exelvi@outlook.com>
2024-07-05 20:07:40 +0200

    chore: Add initial project files and setup Arduino display

---
+++ Peltier_temp.ino
@@ -0,0 +1,39 @@
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 9);
+  display.println("Starting");
+  display.display();
+  delay(250);
+  display.setCursor(0, 9);
+  display.println("Starting.");
+  display.display();
+  delay(250);
+  display.setCursor(0, 9);
+  display.println("Starting..");
+  display.display();
+
+  delay(1000);
+}
+
+void loop()
+{
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Peltier_temp
+ A small and simple project to measure the temperature of a peltier with an OLED display and Arduino
